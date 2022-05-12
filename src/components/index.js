7098aa1dcf1fbb56d7ec1f9b8a251eded238ca93
Takeru9016 <youthfox046@gmail.com>
2022-05-12 12:25:24 +0530

    Completed final stage

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Infobox } from "./Infobox/Infobox";
+export { default as Map } from "./Map/Map";
+export { default as Table } from "./Table/Table";
+export { default as LineGraph } from "./LineGraph/LineGraph";
